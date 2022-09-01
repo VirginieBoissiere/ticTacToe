2b972f80ecfb4a7f3ce438f413f86b0038728d4a
VirginieBoissiere <boissiere.virginie@gmail.com>
2022-09-01 16:10:39 +0200

    Réaliser un composant interactif

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+//NOTE Les composants React peuvent définir un état local en définissant this.state dans leurs constructeurs. this.state est considéré comme une donnée privée du composant React qui le définit. Stockons donc la valeur courante du Square dans this.state, et changeons-la quand on clique sur la case. 
+//NOTE En appelant this.setState depuis un gestionnaire onClick dans la méthode render de Square, nous demandons à React de ré-afficher ce Square dès que le <button> est cliqué. Après la mise à jour, la this.state.value du Square sera 'X', et nous verrons donc X sur le plateau de jeu. Si vous cliquez sur n’importe quel carré, un X devrait s’y afficher.
+constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-      {/*  on modifie le code pour passer une prop appelée value au Square */}
-       {this.props.value}
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
@@ -16,8 +26,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    // on affihe un nombre dans chaque carré affiché
-    return <Square value={i} />; 
+    return <Square />;
   }
 
   render() {
