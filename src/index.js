f0d7e0bbc4fec43f2580aeaa9422103600b53d4c
VirginieBoissiere <boissiere.virginie@gmail.com>
2022-09-02 15:58:52 +0200

    Jouer tour à tour

--- src/index.js
+++ src/index.js
@@ -17,16 +17,23 @@ class Board extends React.Component {
   constructor(props){
     super(props);
       this.state ={
-        squares:Array(9).fill(null)
+        squares:Array(9).fill(null),
+  //Chaque fois qu’un joueur interviendra, xIsNext (un booléen) sera basculé afin de déterminer à qui appartiendra le prochain tour, et l’état du jeu sera sauvegardé
+        xIsNext:true,
       };
 
   }
+  
   // on definie le handle click de la fonction onClick du square 
-  handleClick(i){
+  handleClick(i) {
     // on utilise slice pour créer une copie du tableau squares a modifier au lieu de travailler sur le tableau initial
-    const squares =this.state.squares.slice();
-    squares[i] = 'x';
-    this.setState({squares:squares});
+    const squares = this.state.squares.slice();
+    // on modifie pour alterner les X et O
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
   renderSquare(i) {
     // on modifie la méthode pour qu'elle lise l'information du state
@@ -36,7 +43,8 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    // pour afficher prochain jour X ou O
+    const status = 'prochain joueur:'+ (this.state.xIsNext ? 'x': 'o');
 
     return (
       <div>
